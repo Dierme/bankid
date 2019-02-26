5ea21a3847e956a4a83a69fc6efc2782387cd247
vagrant <vagrant@homestead>
2019-02-26 08:43:33 +0000

    Initial commit

---
+++ Dierme/BankId/BankIdService.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Oleksii
+ * Date: 26/02/2019
+ * Time: 09:41
+ */
+
+namespace Dierme\BankId;
+
+
+class BankIdService
+{
+    public function hello()
+    {
+        print_r('hi');
+    }
+}
