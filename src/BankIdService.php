ee6f865c7a1c119fa495885f0294a08636c5aebc
Oleksii Prykhodko <kalimdor07@gmail.com>
2019-03-04 05:53:05 +0100

    service

--- src/BankIdService.php
+++ src/BankIdService.php
@@ -11,6 +11,7 @@ namespace BankId;
 
 use BankId\Exceptions\ValidationException;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 class BankIdService
 {
@@ -39,4 +40,78 @@ class BankIdService
 
         $this->Client = new Client($client_options);
     }
+
+
+    /**
+     * @param string $personalNumber The personal number of the user. String. 12 digits. Century must be included.
+     * @return array
+     * @throws ClientException
+     */
+    public function getAuthResponse($personalNumber)
+    {
+        $parameters = [
+            'personalNumber' => $personalNumber,
+            'endUserIp' => $this->server_ip,
+            'requirement' => [
+                'allowFingerprint' => true,
+            ],
+        ];
+        $responseData = $this->Client->post('auth', ['json' => $parameters]);
+        $response = $this->decodeBody($responseData);
+
+        return $response;
+    }
+
+
+    /**
+     * @param string $personal_number The personal number of the user. String. 12 digits. Century must be included.
+     * @param string $user_visible_data The text to be displayed and signed.
+     * @param string $user_hidden_data Data not displayed to the user
+     * @return OrderResponse
+     * @throws ClientException
+     */
+    public function getSignResponse(string $personal_number, $user_visible_data, $user_hidden_data = '')
+    {
+
+
+        $parameters = [
+            'personalNumber' => $personal_number,
+            'endUserIp' => $this->server_ip,
+            'userVisibleData' => base64_encode($user_visible_data),
+            'requirement' => [
+                'allowFingerprint' => true,
+            ],
+        ];
+        if (!empty($userHiddenData)) {
+            $parameters['userNonVisibleData'] = base64_encode($user_hidden_data);
+        }
+        $responseData = $this->Client->post('sign', ['json' => $parameters]);
+
+        return new OrderResponse($responseData);
+    }
+
+
+    /**
+     * @param string $orderRef Used to collect the status of the order.
+     * @return CollectResponse
+     * @throws ClientException
+     */
+    public function collectResponse($orderRef)
+    {
+        $responseData = $this->Client->post('collect', ['json' => ['orderRef' => $orderRef]]);
+        return new CollectResponse($responseData);
+    }
+
+
+    /**
+     * @param string $orderRef Used to collect the status of the order.
+     * @return bool
+     * @throws ClientException
+     */
+    public function cancelOrder($orderRef)
+    {
+        $responseCode = $this->Client->post('cancel', ['json' => ['orderRef' => $orderRef]])->getStatusCode();
+        return $responseCode === 200;
+    }
+
 }
